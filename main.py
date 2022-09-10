e4b9700d49ad6d8a6d9045e6f74301f874af8476
Panique <102300552+CLaPanique@users.noreply.github.com>
2022-09-10 21:16:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-from flask import *
-import os
-import aminofix
-import heroku3
-os.system('set FLASK_ENV=development')
+from flask import Flask, request, jsonify 
+import samino
 app = Flask(__name__)
 
-def restart(key, name):
-    heroku_conn = heroku3.from_key(key)
-    botapp = heroku_conn.apps()[name]
-    botapp.restart()
-
-@app.route('/api/login',  methods = ['POST'])
-def get_timezone():
-    try:
-        data = request.form
-        login = data.get("login")
-        password = data.get("password")
-        device = data.get("device")
-        key = data.get("key")
-        name = data.get("name")
-        client = aminofix.Client(device)
-        client.login(email = login, password = password)
-        return f"{client.sid}"
-    except:
-        try:
-            restart(key, name)
-            return abort(404)
-        except:
-            return abort(404)
-
-
-
-if __name__ == '__main__':
-  app.run("0.0.0.0", 5000)
+@app.route('/login', methods = ['POST'])
+def login():
+  data = request.form
+  email = data["email"]
+  password = data["password"]
+  device = data["device"]
+  client = samino.Client(device)
+  login = client.login(email,password).json
+  return jsonify(login)
